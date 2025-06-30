0559dbde913856192c16ba936c20d6e485601f40
Babu, George SDPR:EX <George.Babu@gov.bc.ca>
2025-06-30 11:42:14 -0700

    Deleted NodeJs Express project and created NextJs project

--- social-middleware/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import healthRoutes from './routes/health';
-
-const app = express();
-
-app.use(express.json());
-app.use('/api', healthRoutes);
-
-export default app;
