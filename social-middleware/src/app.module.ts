bbdb25fb4c7fd8b706a90bcfea0cc1a947363df4
Babu, George SDPR:EX <George.Babu@gov.bc.ca>
2025-07-07 13:49:02 -0700

    Implement basic logging using Pino

--- social-middleware/src/app.module.ts
+++ social-middleware/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
 import { DatabaseModule } from './database/database.module';
 
@@ -7,6 +8,26 @@ import { DatabaseModule } from './database/database.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        pinoHttp: {
+          level: config.get('NODE_ENV') === 'production' ? 'info' : 'debug',
+          transport:
+            config.get('NODE_ENV') !== 'production'
+              ? {
+                  target: 'pino-pretty',
+                  options: {
+                    colorize: true,
+                    translateTime: 'SYS:standard',
+                    ignore: 'pid,hostname',
+                  },
+                }
+              : undefined,
+        },
+      }),
     }),
     HealthModule,
     DatabaseModule,
--- social-middleware/src/health/health.controller.ts
+++ social-middleware/src/health/health.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
 
 @Controller('health')
 export class HealthController {
+  constructor(private readonly logger: PinoLogger) { }
   @Get()
   getHealth() {
+    this.logger.info('Health check endpoint hit');
+
     return {
       status: 'ok',
       service: process.env.APP_NAME || 'unknown',
