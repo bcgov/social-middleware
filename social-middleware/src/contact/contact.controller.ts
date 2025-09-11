e95534c8cce545bbf62c1c036f5c4e415d657e9f
Babu, George SDPR:EX <George.Babu@gov.bc.ca>
2025-09-11 08:44:03 -0700

    8473: Implement validationpipe in contact controller

--- social-middleware/src/contact/contact.controller.ts
+++ social-middleware/src/contact/contact.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
 import { ContactService } from './contact.service';
+import { GetContactQueryDto } from './dto/get-contact-query.dto';
 
 @Controller('contact')
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
   @Get('get')
-  async getContact(@Query('birthDate') birthDate: string) {
-    return this.contactService.getContactByBirthDate(birthDate);
+  async getContact(
+    @Query(new ValidationPipe({ whitelist: true, transform: true }))
+    query: GetContactQueryDto,
+  ) {
+    return this.contactService.getContactByBirthDate(query.birthDate);
   }
 }
