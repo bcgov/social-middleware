36af961708322df56090a12d1d4bdfe642d08246
TGunderson <tgunderson@gmail.com>
2025-09-12 18:15:17 -0700

    application package enums

---
+++ social-middleware/src/application-package/enums/application-package-status.enum.ts
@@ -0,0 +1,9 @@
+export enum ApplicationPackageStatus {
+  DRAFT = 'Draft',
+  AWAITING = 'Awaiting Household',
+  READY = 'Ready',
+  SUBMITTED = 'Submitted',
+  RETURNED = 'Returned',
+  WITHDRAWN = 'Withdrawn',
+  ARCHIVED = 'Archived',
+}
