1d19f3aed0359e6bb8695771247b5a8035097f29
TGunderson <tgunderson@gmail.com>
2025-09-12 18:14:45 -0700

    application package schema

---
+++ social-middleware/src/application-package/schema/application-package.schema.ts
@@ -0,0 +1,50 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import {
+  ApplicationPackageSubType,
+  ApplicationPackageSubSubType,
+} from '../enums/application-package-subtypes.enum';
+import { ApplicationPackageStatus } from '../enums/application-package-status.enum';
+
+export type ApplicationPackageDocument = ApplicationPackage & Document;
+
+@Schema({ timestamps: true })
+export class ApplicationPackage {
+  @Prop({ required: true, unique: true })
+  applicationPackageId!: string;
+
+  @Prop({
+    required: true,
+    enum: ApplicationPackageSubType,
+    default: ApplicationPackageSubType.FCH,
+  })
+  subtype!: ApplicationPackageSubType;
+
+  @Prop({
+    required: true,
+    enum: ApplicationPackageSubSubType,
+    default: ApplicationPackageSubSubType.FCH,
+  })
+  subsubtype!: ApplicationPackageSubSubType;
+
+  @Prop({ required: false })
+  srId!: string;
+
+  @Prop({
+    required: true,
+    enum: ApplicationPackageStatus,
+  })
+  status!: ApplicationPackageStatus;
+
+  @Prop({ required: false, default: () => Date() })
+  createdAt!: Date;
+
+  @Prop({ required: false, default: null })
+  submittedAt!: Date;
+
+  @Prop({ required: false, default: () => Date() })
+  updatedAt!: Date;
+}
+
+export const ApplicationPackageSchema =
+  SchemaFactory.createForClass(ApplicationPackage);
