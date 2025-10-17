255273c29a625cdcdf219897b7978d335ef8fcda
TGunderson <tgunderson@gmail.com>
2025-10-17 11:03:46 -0700

    refactor dev-tools to delete application-package

--- social-middleware/src/dev-tools/dev-tools.module.ts
+++ social-middleware/src/dev-tools/dev-tools.module.ts
@@ -4,11 +4,15 @@ import { ConfigModule } from '@nestjs/config';
 import { DevToolsController } from './dev-tools.controller';
 import { DevToolsService } from './dev-tools.service';
 import { User, UserSchema } from '../auth/schemas/user.schema';
-import { HouseholdModule } from 'src/household/household.module';
 import {
-  Application,
-  ApplicationSchema,
-} from '../application/schemas/application.schema';
+  ApplicationPackage,
+  ApplicationPackageSchema,
+} from '../application-package/schema/application-package.schema';
+import {
+  ApplicationForm,
+  ApplicationFormSchema,
+} from '../application-form/schemas/application-form.schema';
+import { HouseholdModule } from 'src/household/household.module';
 import {
   FormParameters,
   FormParametersSchema,
@@ -16,14 +20,14 @@ import {
 import {
   ScreeningAccessCode,
   ScreeningAccessCodeSchema,
-} from 'src/application/schemas/screening-access-code.schema';
-
+} from '../application-form/schemas/screening-access-code.schema';
 @Module({
   imports: [
     ConfigModule,
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
-      { name: Application.name, schema: ApplicationSchema },
+      { name: ApplicationPackage.name, schema: ApplicationPackageSchema },
+      { name: ApplicationForm.name, schema: ApplicationFormSchema },
       { name: FormParameters.name, schema: FormParametersSchema },
       { name: ScreeningAccessCode.name, schema: ScreeningAccessCodeSchema },
     ]),
