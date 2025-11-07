e84e95043be03ec379f93ffb1d9f09cfcc6418b3
TGunderson <tgunderson@gmail.com>
2025-11-07 10:47:10 -0800

    added new forms to application

--- social-middleware/src/application-form/enums/application-form-types.enum.ts
+++ social-middleware/src/application-form/enums/application-form-types.enum.ts
@@ -1,10 +1,7 @@
 export enum ApplicationFormType {
   REFERRAL = 'Referral',
   ABOUTME = 'About Me',
-  ADDRESS = 'Current and past addresses',
   HOUSEHOLD = 'My household and support network',
-  HEALTH = 'Health history',
-  HISTORY = 'Caregiving history',
   REFERENCES = 'References',
   CONSENT = 'Consent and agreements',
   SCREENING = 'Screening',
@@ -13,13 +10,10 @@ export enum ApplicationFormType {
 export const FormId: Record<ApplicationFormType, string> = {
   [ApplicationFormType.REFERRAL]: 'CF0001_Referral',
   [ApplicationFormType.ABOUTME]: 'CF0001_AboutMe',
-  [ApplicationFormType.ADDRESS]: 'CF0001_AboutMe',
   [ApplicationFormType.HOUSEHOLD]: 'HOUSEHOLD',
-  [ApplicationFormType.HEALTH]: 'CF0001_AboutMe',
-  [ApplicationFormType.HISTORY]: 'CF0001_AboutMe',
-  [ApplicationFormType.REFERENCES]: 'CF0001_AboutMe',
-  [ApplicationFormType.CONSENT]: 'CF0001_AboutMe',
-  [ApplicationFormType.SCREENING]: 'CF0001_Referral',
+  [ApplicationFormType.REFERENCES]: 'CF0001_References',
+  [ApplicationFormType.CONSENT]: 'CF0001_Consent',
+  [ApplicationFormType.SCREENING]: 'CF0001_AboutMe',
 };
 
 // helper
