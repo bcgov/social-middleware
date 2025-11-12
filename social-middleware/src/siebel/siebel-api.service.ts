5d94f325d3ac554de468075239d6f7f990e9dacd
TGunderson <tgunderson@gmail.com>
2025-11-12 09:16:39 -0800

    removed comments

--- social-middleware/src/siebel/siebel-api.service.ts
+++ social-middleware/src/siebel/siebel-api.service.ts
@@ -5,6 +5,7 @@ import { firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
 import { SiebelAuthService } from './siebel-auth.service';
 import { PinoLogger } from 'nestjs-pino';
+//import { Builder } from 'xml2js';
 
 interface SiebelContactResponse {
   items?: {
@@ -251,6 +252,124 @@ export class SiebelApiService {
     return await this.put(endpoint, payload);
   }
 
+  /*
+  async createForm(attachmentData, formData: string) {
+    const endpoint = '/ICM REST Forms Upsert/DT Form Instance Orbeon Revise/';
+    const payload = {};
+
+    const decodedFormData = JSON.parse(
+      Buffer.from(formData, 'base64').toString('utf-8'),
+    ) as {
+      data: Record<string, unknown>;
+      form_definition: {
+        data: {
+          items?: unknown[];
+        };
+        elements?: unknown[];
+      };
+    };
+*/
+  /**
+   * Apply Kiln Version
+   * Kiln V1 uses data: { items: []}
+   * Kiln V2 uses dataSources []
+   */
+  /*
+    const kilnVersion = Object.keys(
+      decodedFormData.form_definition.data,
+    ).includes('items')
+      ? 1
+      : 2;
+
+    const formDefinitionItems =
+      kilnVersion === 1
+        ? decodedFormData.form_definition.data.items
+        : decodedFormData.form_definition.elements; // This is the field info for form items
+
+    // dateItemsId : This will contain all of the IDs of the date fields
+    // checkboxItemsId : This will contain all of the IDs of the checkbox fields
+    const { dateItemsId, checkboxItemsId, textInfoFields } = this.getFormIds(
+      formDefinitionItems || [],
+    );
+
+    // note: skipped form exceptions for now..
+
+    // Transform the data for XML
+    const simplifiedData = this.fixJSONValuesForXML(
+      decodedFormData.data,
+      dateItemsId,
+      checkboxItemsId,
+      textInfoFields,
+      kilnVersion,
+    );
+
+    // Generate XML Hierarchy
+    const builder = new Builder({
+      xmldec: { version: '1.0' },
+      renderOpts: { pretty: false },
+      rootName: 'root',
+    });
+
+    const xmlHierarchy = builder.buildObject(simplifiedData);
+    if (isFormException) {
+      // If any forms with the correct version (TODO) have been listed as exceptions, then proceed with their form exceptions
+      // If the root needs a differernt name, apply it here. Otherwise use the default "root"
+      if (
+        propertyExists(dictionary, formId, 'rootName') &&
+        propertyNotEmpty(dictionary, formId, 'rootName')
+      ) {
+        builder = new xml2js.Builder({
+          xmldec: { version: '1.0' },
+          renderOpts: { pretty: false },
+          rootName: dictionary[formId]['rootName'],
+        });
+      } else {
+        builder = new xml2js.Builder({
+          xmldec: { version: '1.0' },
+          renderOpts: { pretty: false },
+        });
+      }
+
+      let wrapperJson = truncatedKeysSaveData;
+      // If subRoots exist, wrap the sub-roots around the JSON where the last array object will be closest to JSON and first array object will be closest to root/rootName
+      if (
+        propertyExists(dictionary, formId, 'subRoots') &&
+        propertyNotEmpty(dictionary, formId, 'subRoots')
+      ) {
+        wrapperJson = {};
+        let tempJson = {};
+        const subRootLength = dictionary[formId]['subRoots'].length;
+        for (i = subRootLength; i > 0; i = i - 1) {
+          if (i === subRootLength) {
+            tempJson[dictionary[formId]['subRoots'][i - 1]] =
+              truncatedKeysSaveData;
+          } else {
+            wrapperJson[dictionary[formId]['subRoots'][i - 1]] = tempJson;
+            tempJson = wrapperJson;
+            wrapperJson = {};
+          }
+        }
+        wrapperJson = tempJson;
+      }
+      saveJson['XML Hierarchy'] = builder.buildObject(wrapperJson);
+    } else {
+      builder = new xml2js.Builder({
+        xmldec: { version: '1.0' },
+        renderOpts: { pretty: false },
+      });
+      saveJson['XML Hierarchy'] = builder.buildObject(truncatedKeysSaveData);
+    }
+    //let url = buildUrlWithParams('SIEBEL_ICM_API_HOST', 'fwd/v1.0/data/DT Form Instance Thin/DT Form Instance Thin/' + attachment_id + '/', '');
+    const xml = saveJson['XML Hierarchy'];
+    const xmlSize = Buffer.byteLength(xml, 'utf8'); // size in bytes
+
+    console.log('XML Hierarchy:', xml);
+    console.log('XML Hierarchy length (chars):', xml.length);
+    console.log('XML Hierarchy size (bytes):', xmlSize);
+
+    return await this.put(endpoint, payload);
+  }
+*/
   async createProspect(prospectData: {
     ServiceRequestId: string;
     IcmBcscDid: string;
