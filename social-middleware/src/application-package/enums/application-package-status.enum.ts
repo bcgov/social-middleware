f3a462f1c347f4c83ffb7bd8dba5a2ab22ea58ad
TGunderson <tgunderson@gmail.com>
2025-11-24 09:59:40 -0800

    updated application package status enums

--- social-middleware/src/application-package/enums/application-package-status.enum.ts
+++ social-middleware/src/application-package/enums/application-package-status.enum.ts
@@ -2,8 +2,9 @@ export enum ApplicationPackageStatus {
   DRAFT = 'Draft', // application package has been created
   REFERRAL = 'Referral Requested', // an information session has been requested
   APPLICATION = 'Application', // the full application package is available and being worked on
-  CONSENT = 'Consent', // the application package is 'complete' and is locked while consent is being collected from household members
-  SUBMITTED = 'Submitted', // the application package with required consents has been submitted to ICM
+  CONSENT = 'Consent', // the application package forms and household has been completed. It is locked while consent is being collected from household members
+  READY = 'Ready', // the application package has met the requirements for completion and can be submitted to ICM
+  SUBMITTED = 'Submitted', // the application packagehas been submitted to ICM
   RETURNED = 'Returned', // not yet implemented
   WITHDRAWN = 'Withdrawn', // not yet implemented
   ARCHIVED = 'Archived', // not yet implemented
