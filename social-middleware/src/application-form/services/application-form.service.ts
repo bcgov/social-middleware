0fccfafce1b7d5fbbf7c292e1ea4fb0d50311ef2
chridodd <christopher.dodd@gov.bc.ca>
2025-12-03 18:27:15 +0000

    restored build

--- social-middleware/src/application-form/services/application-form.service.ts
+++ social-middleware/src/application-form/services/application-form.service.ts
@@ -82,8 +82,6 @@ export class ApplicationFormService {
 
       this.logger.info({ applicationFormId }, 'Saved application form to DB');
 
-      const apiUrl = this.configService.get<string>('API_URL') || 'http://localhost:3001';
-
       const newFormDto = {
         applicationFormId: applicationFormId,
         type: FormType.New,
@@ -91,8 +89,6 @@ export class ApplicationFormService {
         formParameters: {
           formId: dto.formId,
           language: 'en',
-          apiUrl: `${apiUrl}/forms/validateTokenAndGetParameters`,
-          apiMethod: 'POST',
         },
       };
 
