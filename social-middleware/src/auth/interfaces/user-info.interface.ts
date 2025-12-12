bb5fd59686cb2ab8709dbb0a961cfbd35f821490
TGunderson <tgunderson@gmail.com>
2025-12-11 18:53:55 -0800

    moved userinfo to interface

---
+++ social-middleware/src/auth/interfaces/user-info.interface.ts
@@ -0,0 +1,16 @@
+export interface UserInfo {
+  sub: string;
+  email: string;
+  name?: string;
+  given_name: string;
+  family_name: string;
+  gender: string;
+  birthdate: string;
+  address: {
+    street_address: string;
+    country: string;
+    region: string;
+    locality: string;
+    postal_code: string;
+  };
+}
