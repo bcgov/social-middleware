011ab17ca7f192e22f12eb8f4e198eef1f6e9749
TGunderson <tgunderson@gmail.com>
2026-02-02 14:51:51 -0800

    registering queue module elements

--- social-middleware/src/notifications/notification.module.ts
+++ social-middleware/src/notifications/notification.module.ts
@@ -3,10 +3,12 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { ChesService } from './services/ches.service';
 import { NotificationService } from './services/notification.service';
+import { NotificationQueueModule } from './queue/notification-queue.module';
+import { NotificationProcessor } from './queue/notification.processor';
 
 @Module({
-  imports: [HttpModule, ConfigModule],
-  providers: [ChesService, NotificationService],
-  exports: [NotificationService],
+  imports: [HttpModule, ConfigModule, NotificationQueueModule],
+  providers: [ChesService, NotificationService, NotificationProcessor],
+  exports: [NotificationService, NotificationQueueModule],
 })
 export class NotificationModule {}
