a83a194c6e03c8151a59c22dc7666ad2b297af68
TGunderson <tgunderson@gmail.com>
2026-02-02 14:52:17 -0800

    updates to bull dashboard module

--- social-middleware/src/bull-dashboard/bull-dashboard.module.ts
+++ social-middleware/src/bull-dashboard/bull-dashboard.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { BullDashboardService } from './bull-dashboard.service';
 //import { ApplicationQueueModule } from '../application/queue/application-queue.module';
 import { ApplicationPackageQueueModule } from '../application-package/queue/application-package-queue.module';
+import { IcmStageQueueModule } from '../application-package/queue/icm-stage-queue.module';
+import { NotificationQueueModule } from '../notifications/queue/notification-queue.module';
 
 @Module({
-  imports: [ApplicationPackageQueueModule],
+  imports: [
+    ApplicationPackageQueueModule,
+    IcmStageQueueModule,
+    NotificationQueueModule,
+  ],
   providers: [BullDashboardService],
   exports: [BullDashboardService],
 })
