82558bb3f13275afd5e1d144f2cf819ca67bcb90
TGunderson <tgunderson@gmail.com>
2026-02-09 11:48:29 -0800

    added more notification types

--- social-middleware/src/notifications/services/notification.service.ts
+++ social-middleware/src/notifications/services/notification.service.ts
@@ -95,33 +95,58 @@ export class NotificationService {
     await this.notificationQueueService.sendEmail(emailData);
   }
 
-    /**
-   * Send access code notification to HOUSEHOLD MEMBER
+  /**
+   * When a household member submits their screening info through the portal, trigger an email to the applicant to let them know.
    */
-    async sendApplicationNotSubmitted(
+  async sendApplicationNotSubmitted(
     email: string,
-      applicantName: string,
-      householdMemberName: string,
-      accessCode: string,
-    ): Promise<void> {
-      const emailData: SendEmailDto = {
-        to: [this.getToEmail(email)],
-        from:
-          this.configService.get<string>('CHES_FROM_EMAIL') ||
-          'noreply@gov.bc.ca',
-        subject: 'Foster Caregiver Screening Request',
-        body: `
-            <h2>You have been named as a household member on a foster caregiver application</h2>
-            <p>Hello ${householdMemberName},</p>
-            <p>${applicantName} has identified you as a household member on their application to become a foster caregiver. As part of the assessment process, the Ministry of Children and Family Development requires each adult household member to provide background information and consent to screening activities.</p>
-            <p>Sign into the caregiver portal using your BC Services Card and use <b>${accessCode}</b> as the access code to begin the process.</p>
+    applicantName: string,
+    householdMemberName: string,
+  ): Promise<void> {
+    const emailData: SendEmailDto = {
+      to: [this.getToEmail(email)],
+      from:
+        this.configService.get<string>('CHES_FROM_EMAIL') ||
+        'noreply@gov.bc.ca',
+      subject: 'Household Consent Provided',
+      body: `
+            <h2>A household member has provided their screening information</h2>
+            <p>Hello ${applicantName},</p>
+            <p>${householdMemberName} has provided their screening and consent information via the caregiver portal.</p>
+            <p>Once all household members have provided their information, your application will be forwarded along for processesing. You can track the status of your household members through the caregiver portal.</p>
             <p>Thank you,<br>BC Caregiver Registry Team</p>
           `,
-        bodyType: 'html',
-        priority: 'normal',
-      };
-      await this.notificationQueueService.sendEmail(emailData);
-    }
+      bodyType: 'html',
+      priority: 'normal',
+    };
+    await this.notificationQueueService.sendEmail(emailData);
+  }
+
+  /**
+   * When all applicable consent/screening records have been provided, notify the applicant that their application has been sent to MCFD
+   */
+  async sendApplicationSubmitted(
+    email: string,
+    applicantName: string,
+  ): Promise<void> {
+    const emailData: SendEmailDto = {
+      to: [this.getToEmail(email)],
+      from:
+        this.configService.get<string>('CHES_FROM_EMAIL') ||
+        'noreply@gov.bc.ca',
+      subject: 'Foster caregiver application submitted to MCFD',
+      body: `
+              <h2>Your foster caregiver application has been submitted</h2>
+              <p>Hello ${applicantName},</p>
+              <p>All outstanding information has been provided for your caregiver application. It has been submitted to the Ministry of Child and Family Development for processing.</p> 
+              <p>Social workers will be in touch in the coming weeks about any outstanding questions they may have. You may proceed to complete your Medical Screening form. Please log into the caregiver portal for information on providing your Medical Screening information.</p>
+              <p>Thank you,<br>BC Caregiver Registry Team</p>
+            `,
+      bodyType: 'html',
+      priority: 'normal',
+    };
+    await this.notificationQueueService.sendEmail(emailData);
+  }
 
   /**
    * Send custom notification
