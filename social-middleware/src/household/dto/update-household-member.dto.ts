81956f0b1307c72a022e6e3753c6114e8914b46e
TGunderson <tgunderson@gmail.com>
2026-03-18 16:02:33 -0700

    dto for updating a household member

---
+++ social-middleware/src/household/dto/update-household-member.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsString,
+  MinLength,
+  MaxLength,
+  IsDateString,
+  IsEmail,
+  ValidateIf,
+} from 'class-validator';
+
+export class UpdateHouseholdMemberDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  lastName!: string;
+
+  @IsDateString()
+  dateOfBirth!: string;
+
+  @ValidateIf((o) => o.email !== '' && o.email != null)
+  @IsEmail({}, { message: 'Please enter a valid email address' })
+  @MaxLength(255)
+  email?: string;
+}
