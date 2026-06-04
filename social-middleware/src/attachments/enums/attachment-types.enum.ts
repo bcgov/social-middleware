4182a9a67eb8f645676b8996b6781dde7f397eed
TGunderson <tgunderson@gmail.com>
2026-06-04 14:46:01 -0700

    update attachment types enum and category mapping

--- social-middleware/src/attachments/enums/attachment-types.enum.ts
+++ social-middleware/src/attachments/enums/attachment-types.enum.ts
@@ -1,13 +1,27 @@
 export enum AttachmentType {
-  MEDICAL_ASSESSMENT = 'Medical Assessment',
-  CRIMINAL_RECORD_CHECK = 'Criminal Record Check',
-  CONSENT = 'Consent',
   _ = '_',
-  REFERENCE_LETTER = 'Reference Letter',
+  ABOUTSPOUSE = 'About Me (Spouse)',
+  CONSENT = 'Consent',
+  CRIMINAL_RECORD_CHECK = 'Criminal Record Check',
+  DISCLOSURECONSENT = 'Consent for Disclosure of Criminal Record Information',
+  MEDICAL_ASSESSMENT = 'Medical Assessment',
   IDENTIFICATION = 'Identification',
+  INTERNATIONAL_RECORD = 'International Criminal Record Check',
+  OTHER = 'Other Document',
+  PCCCONSENT = 'Consent for Prior Contact Check',
   PROOF_OF_RESIDENCE = 'Proof of Residence',
-  OTHER = 'Other',
+  REFERENCE_LETTER = 'Reference Letter',
 }
+
+// meta data for attachments in ICM
+export const AttachmentCategoryMap: Partial<Record<AttachmentType, string>> = {
+  [AttachmentType.MEDICAL_ASSESSMENT]: 'Medical',
+  [AttachmentType.INTERNATIONAL_RECORD]: 'Documentation',
+  [AttachmentType.DISCLOSURECONSENT]: 'Consent',
+  [AttachmentType.PCCCONSENT]: 'Consent',
+  [AttachmentType.ABOUTSPOUSE]: 'Application & Request',
+  [AttachmentType.OTHER]: 'Other Assessment',
+};
 
 export enum AllowedFileType {
   PDF = 'pdf',
